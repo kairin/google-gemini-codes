0b76e7fa0589bb34e02df1385dc70471bac4b0e5
Mister K <678459+kairin@users.noreply.github.com>
2025-06-10 23:16:53 +0000

    fix: implement conditional base path for dev and prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  
-  // These two lines are the important part for GitHub Pages
+
+  // This function ensures the correct paths are used for dev vs. build
+  base: process.env.NODE_ENV === 'production' ? '/google-gemini-codes' : '/',
   site: 'https://kairin.github.io',
-  base: '/google-gemini-codes',
 });
