9a490a95d656272be7b31f0699e99f89db3e2656
Mister K <678459+kairin@users.noreply.github.com>
2025-06-10 23:40:15 +0000

    up up

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
 
-  // This function ensures the correct paths are used for dev vs. build
-  base: process.env.NODE_ENV === 'production' ? '/google-gemini-codes' : '/',
+  // This section defines the configuration for your project.
+  // The GitHub Actions workflow will OVERRIDE these during the deployment build,
+  // but they are essential for local development and for other tools to understand your site structure.
   site: 'https://kairin.github.io',
+  base: '/google-gemini-codes',
 });
