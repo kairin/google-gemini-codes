b225b2269aac6910fbf02f38423e75cabc2455fe
Mister K <678459+kairin@users.noreply.github.com>
2025-06-10 23:01:18 +0000

    feat: Add Astro deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind()],
+  
+  // These two lines are the important part for GitHub Pages
+  site: 'https://kairin.github.io',
+  base: '/google-gemini-codes',
 });
-
