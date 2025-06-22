38c72ca5423c7d5722e52ecccad78f490d7036a7
Mister K <678459+kairin@users.noreply.github.com>
2025-06-22 05:49:59 +0000

    Update .gitignore and configure Astro settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+
+export default defineConfig({
+  // Replace with your GitHub username and repo name
+  site: 'https://kairin.github.io',
+  base: '/google-gemini-codes',
+  output: 'static',
+});
